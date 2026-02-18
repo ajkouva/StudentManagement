199885c86d7dcf010a0b9b3c4266b80c5f99916a
Shivam bisht <shivambisht02006@gmail.com>
2026-02-18 14:37:37 +0530

    update from colab

--- frontend/loginPage/script.js
+++ frontend/loginPage/script.js
@@ -1,36 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-   
-    const studentBtn = document.getElementById('student-btn');
-    const teacherBtn = document.getElementById('teacher-btn');
-    const loginTitle = document.getElementById('login-title');
-    const stduent_content=document.getElementById('stduent_content');
-    const boxcontent2=document.querySelector(".In-box2");
-   
-    teacherBtn.classList.add('active');
 
-    
-    studentBtn.addEventListener('click', () => {
-        setActiveUser('student');
-    });
-
-    teacherBtn.addEventListener('click', () => {
-        setActiveUser('teacher');
-    });
-
-    function setActiveUser(type) {
-        if (type === 'student') {
-            studentBtn.classList.add('active');
-            teacherBtn.classList.remove('active');
-            loginTitle.textContent = 'Student Login';
-            stduent_content.innerHTML='Welcome to <br>student portal';
-            boxcontent2.style.backgroundImage='url("assets/image1.png")';
-           
-        } else {
-            teacherBtn.classList.add('active');
-            studentBtn.classList.remove('active');
-            loginTitle.textContent = 'Teacher Login';
-            stduent_content.innerHTML='Welcome to <br>teacher portal';
-            boxcontent2.style.backgroundImage='url("assets/image.png")';
-        }
+const EmailE1=document.querySelector(".Email");
+const PasswordE1=document.querySelector(".Password");
+const loginBtn=document.querySelector("#login-btn");
+loginBtn.addEventListener("click",(e)=>{
+    loginwork();
+})
+async function loginwork(){
+    const Email=EmailE1.value;
+    const Password=PasswordE1.value;
+    try {
+        const rse = await fetch("http://localhost:3000/api/auth/login",{
+            "Email":Email,
+            "Password":Password
+        },{
+            method:"POST",
+             headers: {
+                "Content-Type": "application/json",
+        }})
+        const data = await rse.json();
+        console.log(data);
+    } catch (error) {
+        
     }
-});
+     
+}
