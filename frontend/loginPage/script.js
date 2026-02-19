8b004037996ffd380cf666626e047687a8016fd5
shivamtadiyal1212-creator <shivamtadiyal1212@gmail.com>
2026-02-19 10:31:51 +0530

    Update login page files

--- frontend/loginPage/script.js
+++ frontend/loginPage/script.js
@@ -2,12 +2,16 @@
 const EmailE1=document.querySelector(".Email");
 const PasswordE1=document.querySelector(".Password");
 const loginBtn=document.querySelector("#login-btn");
+const Email=EmailE1.value;
+const Password=PasswordE1.value;
+
 loginBtn.addEventListener("click",(e)=>{
+    checkpassword();
     loginwork();
 })
 async function loginwork(){
-    const Email=EmailE1.value;
-    const Password=PasswordE1.value;
+    
+    console.log("hello")
     try {
         const rse = await fetch("http://localhost:3000/api/auth/login",{
             "Email":Email,
@@ -23,4 +27,12 @@ async function loginwork(){
         
     }
      
+}
+function checkpassword(){
+
+    if(Email.trim()===""|| Password.trim()===""){
+
+        alert("email or password should not be empty");
+        return false;
+    }
 }
